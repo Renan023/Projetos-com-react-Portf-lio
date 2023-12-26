8833da2a98f537a6e2dc5cd5939705ad0a605b33
Renan <104283869+Renan023@users.noreply.github.com>
2023-12-25 21:22:32 -0300

    Add files via upload

--- page/Coffee.js
+++ page/Coffee.js
@@ -3,22 +3,22 @@ import Welcome from '../Coffee/Welcome'
 import Inputradio from '../Coffee/Inputrad'
 import TitleCoffee from '../Coffee/Titlecoffee'
 import WWA from '../Coffee/Whoweare'
-import Space from '../Coffee/Space'
 import Harmonious from '../Coffee/HarmoniousCoffeee'
-import Row from '../Coffee/Row'
+import AudioPlayer from '../Coffee/Music'
+import Time from '../Components/Time'
 
 function CoffeeCo(){
 
     return(
         <div className='imgcoffee'>
             <div id='coffee'>
+                <AudioPlayer/>
+                <Time/>
                 <TitleCoffee title='Coffee Co.'/>
                 <Welcome/>
                 <Inputradio/>
                 <WWA/>
-                <Space/>
                 <Harmonious/>
-                <Row/>
             </div>
         </div>
     )
